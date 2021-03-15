b1ecfd3cd58782559d1c222b789986219bbb9290
XJP <1017334958@qq.com>
2021-03-15 22:38:44 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,26 @@
 
 let baseURL = 'http://api-breakingnews-web.itheima.net';
 
-$.ajaxPrefilter(function (params) {
+$.ajaxPrefilter(function (options) {
 
-    params.url = baseURL + params.url;
+    options.url = baseURL + options.url;
+
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token' || '')
+        }
+    }
+
+    options.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+
+        }
+    }
+
+
 })
